5703b72f1fa7622896b0a939b60ddcf91de766dc
Ryohei Ueda <garaemon@gmail.com>
2017-10-21 15:04:44 +0900

    Moving the Camera

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// LateUpdate is called per frame but after all the Update() is called
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
